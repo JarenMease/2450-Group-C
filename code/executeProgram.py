3b93eb60dfabb08dd5d97d01e930e2c9880e180d
BrandonWoodruff <bmw132000@gmail.com>
2024-02-18 20:23:57 -0700

    This is all of my updates hopefully I didn't break anything...

---
+++ code/executeProgram.py
@@ -0,0 +1,139 @@
+from main import *
+
+
+class Program():
+  
+    def __init__(self):
+        self._memory = [0] * 100
+        self._accumulator = 0
+        self._pc = 0
+        self._operand = 0
+        
+    
+    def readMlProgram(self):
+        self._file = input("Enter the name of the input file: ")
+        try:
+            with open(self._file, 'r') as f:
+                self._program = f.readlines()
+        except FileNotFoundError:
+            print("File not found. Please try again.")
+            self.readMlProgram()
+    
+    def execute_program(self):
+        self._pc = 0
+        while self._pc < len(self._program):
+            self._op, self._operand = self._program[self._pc]
+            self._operand = int(self._operand)
+            match self._op:
+              case 10: #read
+                  self.read()
+              case 11: #write
+                  self.write()
+              case 20: #load
+                  self._accumulator = self.load()
+              case 21: #store
+                  self.store()
+              case 30: #add
+                  self._accumulator = self.add()
+              case 31: #subtract
+                  self._accumulator = self.subtract()
+              case 32: #divide
+                  self._accumulator = self.divide()
+              case 33: #multiply
+                  self._accumulator = self.multiply()
+              case 40: #branch
+                  self._pc = self.branch()
+              case 41: #branchNeg
+                  self._pc = self.branchNeg()
+              case 42: #branchZero
+                  self._pc = self.branchZero()
+              case 43: #halt
+                  print("Result: ", self._accumulator)
+                  print("Program halted.")
+                  break
+              case _: 
+                  print(f"Unknown operation: {self._op}")
+                  print("Program halted.")
+                  break
+            self._pc += 1
+    
+      
+    def read(self):
+      while True:
+          try:
+            value = int(input(f"What number would you like read into memory location {self._operand}? "))
+            self._memory[self._operand] = value
+            break
+          except ValueError:
+            print("Please enter a valid number.")
+            continue
+    
+    def write(self):
+      print(self._memory[self._operand])
+      
+    def load(self):
+      return self._memory[self._operand]
+    
+    def store(self):
+      self._memory[self._operand] = self._accumulator
+      
+    def add(self):
+      return self._accumulator + self._memory[self._operand]
+    
+    def subtract(self):
+      return self._accumulator - self._memory[self._operand]
+    
+    def divide(self):
+      if self._memory[self._operand] == 0:
+          print("Did not divide. Zero division error.")
+          return self._accumulator
+      return self._accumulator // self._memory[self._operand]
+    
+    def multiply(self):
+      return self._accumulator * self._memory[self._operand]
+    
+    def branch(self):
+      return self._operand
+    
+    def branchNeg(self):
+      if self._accumulator < 0:
+          return self._operand
+      else:
+          return self._pc
+        
+    def branchZero(self):
+      if self._accumulator == 0:
+          return self._operand
+      else:
+          return self._pc
+        
+    def set_accumulator(self, value):
+      self._accumulator = value
+      
+    def get_accumulator(self):
+      return self._accumulator
+    
+    def set_memory(self, index, value):
+      self._memory[index] = value
+      
+    def get_memory(self, index):
+      return self._memory[index]
+    
+    def set_pc(self, value):
+      self._pc = value
+    
+    def get_pc(self):
+      return self._pc
+    
+    def set_operand(self, value):
+      self._operand = value
+      
+    def get_operand(self):
+      return self._operand
+    
+    def set_program(self, program):
+      self._program = program
+    
+    
+    
+    
--- code/test_main.py
+++ code/test_main.py
@@ -1,171 +1,174 @@
 import pytest
-
 from main import *
+from executeProgram import *
 
 def test_always_passes():
     assert True
 
 # def test_always_fail():
 #     assert False
-
-    
+   
 def test_load():
-    memory = [0] * 100
-    memory[0] = 5
-    operand = 0
-    accumulator = load(operand, memory)
-    assert accumulator == 5
+    program = Program()
+    program.set_memory(0, 10)
+    accumulator = program.load()
+    assert accumulator == 10
 
 def test_neg_load():
-    memory = [0] * 100
-    memory[0] = -5
-    operand = 0
-    accumulator = load(operand, memory)
+    program = Program()
+    program.set_memory(0, -5)
+    accumulator = program.load()
     assert accumulator == -5
     
 def test_store():
-    memory = [0] * 100
-    accumulator = 50
-    operand = 0
-    store(accumulator, operand, memory)
-    assert memory[0] == 50
+    program = Program()
+    program.set_memory(0, 10)
+    program.set_accumulator(50)
+    program.store()
+    storing = program.get_memory(0)
+    assert storing == 50
 
 def test_neg_store():
-    memory = [0] * 100
-    accumulator = -50
-    operand = 0
-    store(accumulator, operand, memory)
-    assert memory[0] == -50
+    program = Program()
+    program.set_memory(0, -10)
+    program.set_accumulator(-50)
+    program.store()
+    storing = program.get_memory(0)
+    assert storing == -50
 
 def test_multiply():
-    memory = [0] * 100
-    memory[0] = 4
-    accumulator = 10
-    operand = 0
-    result = multiply(accumulator, operand, memory)
-    assert result == 40
+    program = Program()
+    program.set_accumulator(10)
+    program.set_memory(0, 5)
+    result = program.multiply()
+    assert result == 50
 
 def test_neg_multiply():
-    memory = [0] * 100
-    memory[0] = -4
-    accumulator = 10
-    operand = 0
-    result = multiply(accumulator, operand, memory)
-    assert result == -40
+    program = Program()
+    program.set_accumulator(-10)
+    program.set_memory(0, 5)
+    result = program.multiply()
+    assert result == -50
     
 def test_divide():
-    memory = [0] * 100
-    memory[0] = 4
-    accumulator = 10
-    operand = 0
-    result = divide(accumulator, operand, memory)
+    program = Program()
+    program.set_accumulator(10)
+    program.set_memory(0, 5)
+    result = program.divide()
     assert result == 2
 
 def test_zero_divide():
-    memory = [0] * 100
-    memory[0] = 0
-    accumulator = 10
-    operand = 0
-    result = divide(accumulator, operand, memory)
-    assert result == 10 
+    program = Program()
+    program.set_accumulator(10)
+    program.set_memory(0, 0)
+    result = program.divide()
+    assert result == 10
 
 def test_neg_divide():
-    memory = [0] * 100
-    memory[0] = -4
-    accumulator = 10
-    operand = 0
-    result = divide(accumulator, operand, memory)
-    assert result == -3
+    program = Program()
+    program.set_accumulator(-10)
+    program.set_memory(0, 5)
+    result = program.divide()
+    assert result == -2
 
 def test_divide_less_one():
-    memory = [0] * 100
-    memory[0] = 10
-    accumulator = 4
-    operand = 0
-    result = divide(accumulator, operand, memory)
+    program = Program()
+    program.set_accumulator(1)
+    program.set_memory(0, 5)
+    result = program.divide()
     assert result == 0
     
 def test_branch():
-    operand = 5
-    result = branch(operand)
+    program = Program()
+    program.set_operand(5)
+    result = program.branch()
     assert result == 5
-
+    
 def test_branch2():
-    program = [(40,3), (00,00), (00,00), (20,00), (43,00)]
-    memory = [0] * 100
-    memory[0] = 10
-    accumulator = 0
-    memory, accumulator = execute_program(program, memory, accumulator)
-    assert accumulator == 10
-    
-    
+    program = Program()
+    program.set_pc(10)
+    program.set_operand(15)
+    result = program.branch()
+    assert result == 15
+        
 def test_branchNeg():
-    operand = 20
-    pc = 0
-    accumulator = -5
-    result = branchNeg(accumulator, operand, pc)
+    program = Program()
+    program.set_operand(20)
+    program.set_accumulator(-5)
+    result = program.branchNeg()
     assert result == 20
     
 def test_branchNeg2():
-    operand = 5
-    pc = 0
-    accumulator = 5
-    result = branchNeg(accumulator, operand, pc)
+    program = Program()
+    program.set_operand(5)
+    program.set_accumulator(5)
+    result = program.branchNeg()
     assert result == 0
 
 def test_branchZero():
-    operand = 25
-    pc = 5
-    accumulator = 0
-    result = branchZero(accumulator, operand, pc)
+    program = Program()
+    program.set_operand(25)
+    program.set_pc(5)
+    result = program.branchZero()
     assert result == 25
     
 def test_branchZero2():
-    operand = 5
-    pc = 0
-    accumulator = 5
-    result = branchZero(accumulator, operand, pc)
+    program = Program()
+    program.set_accumulator(5)
+    result = program.branchZero()
     assert result == 0
     
-def test_read_program():
-    file_path = 'programtest.txt'
-    program = read_ml_program(file_path)
-    assert program == [(10,00), (11,55), (20,00), (31, 55), (32, 66)]
-    
-def test_read_program2():
-    file_path = 'programtest2.txt'
-    with pytest.raises(ValueError):
-        program = read_ml_program(file_path)
-    assert ValueError(f"Command +29938 is more or less than 4 digits. Please correct the program txt file.")
-    
 def test_add():
-    memory = [0] * 100
-    memory[0] = 5
-    accumulator = 5
-    operand = 0
-    result = add(accumulator, operand, memory)
+    program = Program()
+    program.set_accumulator(5)
+    program.set_memory(0, 5)
+    result = program.add()
     assert result == 10
 
 def test_add2():
-    program = [(30, 5), (43,00)]
-    accumulator = 5
-    memory = [0] * 100
-    memory[5] = 5
-    memory, accumulator = execute_program(program, memory, accumulator)
-    assert accumulator == 10
+    program = Program()
+    program.set_accumulator(5)
+    program.set_memory(5, 5)
+    program.set_program([(30, 5), (43,00)])
+    program.execute_program()
+    result = program.get_accumulator()
+    assert result == 10
+    # program = [(30, 5), (43,00)]
+    # accumulator = 5
+    # memory = [0] * 100
+    # memory[5] = 5
+    # memory, accumulator = execute_program(program, memory, accumulator)
+    # assert accumulator == 10
     
 def test_subtract():
-    memory = [0] * 100
-    memory[0] = 5
-    accumulator = 10
-    operand = 0
-    result = subtract(accumulator, operand, memory)
+    program = Program()
+    program.set_accumulator(10)
+    program.set_memory(0, 5)
+    result = program.subtract()
     assert result == 5
     
 def test_subtract2():
-    program = [(31, 5), (43,00)]
-    accumulator = 10
-    memory = [0] * 100
-    memory[5] = 5
-    memory, accumulator = execute_program(program, memory, accumulator)
-    assert accumulator == 5
+    program = Program()
+    program.set_accumulator(10)
+    program.set_memory(5, 5)
+    program.set_program([(31, 5), (43,00)])
+    program.execute_program()
+    result = program.get_accumulator()
+    assert result == 5
+    
+def test_execute_program():
+    program = Program()
+    program.set_memory(5, 5)
+    program.set_program([(20, 5), (30, 5), (43, 00)])
+    program.execute_program()
+    result = program.get_accumulator()
+    assert result == 10
+
+def test_execute_program2():
+    program = Program()
+    program.set_memory(5, 10)
+    program.set_memory(10, 5)
+    program.set_program([(20, 5), (31, 10), (43, 00)])
+    program.execute_program()
+    result = program.get_accumulator()
+    assert result == 5
