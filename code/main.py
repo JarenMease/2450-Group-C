1be58019efd2887d578ff5d1ab90815880a9c77a
jredisme <jared.inbox@gmail.com>
2024-02-19 22:40:21 -0700

    Update main.py my_Sim.execute()

--- code/main.py
+++ code/main.py
@@ -5,7 +5,7 @@ def main():
     my_Sim = execute_program()
     my_Sim.read_ml_program() #should rename to read_ml_program BW Need to go to the front end
     my_Sim._memory.load_ml_program(my_Sim._program)
-    my_Sim.execute_program.execute()
+    my_Sim.execute()
 
 if __name__ == "__main__":
     main()
