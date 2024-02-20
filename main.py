142ee23386fb1fd984a2bd5547760acd8f76c2b1
jredisme <jared.inbox@gmail.com>
2024-02-19 22:43:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from execute_program import UVSim
+from execute_program import *
+from uvsim import *
 
 def main():
-    my_Sim = UVSim()
-    my_Sim.read_ml_program()
-    my_Sim.load_ml_program()
-    my_Sim.execute_program()
+    my_Sim = execute_program()
+    my_Sim.read_ml_program() #should rename to read_ml_program BW Need to go to the front end
+    my_Sim._memory.load_ml_program(my_Sim._program)
+    my_Sim.execute()
 
 if __name__ == "__main__":
     main()
