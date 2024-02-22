7e1a7f551413ed4fe1ffc576410e48fe43a8f5ee
jredisme <jared.inbox@gmail.com>
2024-02-22 09:31:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from execute_program import *
-from uvsim import *
+from uvsim import UVSim
+from gui import SimpleGUI
 
 def main():
-    my_Sim = execute_program()
-    my_Sim.read_ml_program() #should rename to read_ml_program BW Need to go to the front end
-    my_Sim._memory.load_ml_program(my_Sim._program)
-    my_Sim.execute()
+    my_sim = UVSim(100)
+    my_gui = SimpleGUI(my_sim) 
+
+    
+    #mainloop has to be the last code line, because that closes the GUI
+    #So anything we want to do has to be done in the GUI itself
+    #So, we actually need to pass sim to gui
+        #And gui will handle the execution of everything
+            #Because gui does input and output
+            #But we pass it a brain (functions that affect memory) from UVSim
+    my_gui.main.mainloop()
 
 if __name__ == "__main__":
     main()
