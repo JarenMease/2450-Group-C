a525d710124fd6d1e02b7f3c574622f936ffe0a5
jredisme <jared.inbox@gmail.com>
2024-02-22 12:33:37 -0700

    Update main.py with comments

--- main.py
+++ main.py
@@ -2,17 +2,10 @@ from uvsim import UVSim
 from gui import SimpleGUI
 
 def main():
-    my_sim = UVSim(100)
-    my_gui = SimpleGUI(my_sim) 
-
-    
-    #mainloop has to be the last line, because that closes the GUI
-    #So anything we want to do has to be done in the GUI itself
-    #So, we actually need to pass sim to gui
-        #And gui will handle the execution of everything
-            #Because gui does input and output
-            #But we pass it a brain (functions that affect memory) from UVSim
-    my_gui.main.mainloop()
+    '''Run machine language simulator'''
+    my_sim = UVSim(100)  # initialize simulator with 100 memory registers
+    my_gui = SimpleGUI(my_sim) # intialize gui, pass sim to gui
+    my_gui.main.mainloop() #loops through GUI operations while GUi is open
 
 if __name__ == "__main__":
     main()
