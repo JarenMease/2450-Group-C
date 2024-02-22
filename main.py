b3738e69a71b8d69c48997a633ba8af62af60ba7
jredisme <jared.inbox@gmail.com>
2024-02-22 11:53:59 -0700

    Update main.py working proto

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     my_gui = SimpleGUI(my_sim) 
 
     
-    #mainloop has to be the last code line, because that closes the GUI
+    #mainloop has to be the last line, because that closes the GUI
     #So anything we want to do has to be done in the GUI itself
     #So, we actually need to pass sim to gui
         #And gui will handle the execution of everything
